fdc2bae2d765d78b5840db334b5c10b8e5ecfb39
Javier Gutierrez <3024021520101@ingenieria.usac.edu.gt>
2025-06-10 23:47:09 -0600

    Creating Interfaces
    Creating interfaces acording to JSON

---
+++ src/constants/interfaces.ts
@@ -0,0 +1,51 @@
+export interface Poster {
+  url: string;
+  aspectRatio: string;
+}
+
+export interface CastMember {
+  characterName: string;
+  actorName: string;
+}
+
+export interface Crew {
+  directors: string[];
+  producers: string[];
+  writers: string[];
+}
+
+export interface Classification {
+  rating: string;
+  advisoryContent: string[];
+}
+
+export interface MovieItem {
+  id: string;
+  title: string;
+  year: number;
+  duration: string;
+  rating: string;
+  quality: string;
+  description: string;
+  isTopMovie: boolean;
+  similarContent: string[];
+  posters: {
+    portrait: Poster;
+    landscape: Poster;
+    thumbnail: Poster;
+  };
+  cast: CastMember[];
+  crew: Crew;
+  classification: Classification;
+}
+
+export interface Container {
+  id: string;
+  title: string;
+  layout: 'portrait-card' | 'landscape-card' | 'thumbnail-card';
+  items: MovieItem[];
+}
+
+export interface ApiResponse {
+  containers: Container[];
+}
